1c4700ac15bcd0efee849d1983302fcd0df9e063
Rafael Santos <rafa.05125@gmail.com>
2024-04-04 23:37:03 -0300

    ValidateString

---
+++ Calculator/Services/ValidateString.cs
@@ -0,0 +1,12 @@
+namespace Calculator.Services
+{
+
+    public class ValidateString
+    {
+        public int CountCaracters(string text)
+        {
+            int num = text.Length;
+            return num;
+        }
+    }
+}
